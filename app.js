714b2367722ab9e11d3b695e486ccb9f7f45d990
Tushar Sharma <61770997+tusharei@users.noreply.github.com>
2024-07-22 18:02:40 +0530

    test

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require("path")
+const http = require("http");
+
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function(socket){
+    socket.on("send-location", function(data) {
+        io.emit("receive-location", {id: socket.id, ...data});
+    });
+    socket.on("disconnect", function(){
+        io.emit("user-disconnected", socket.io);
+    });
+});
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
